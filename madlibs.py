8a0e9fbeec6d4b5fdf215a7a75d8c03ac913168e
almost-good <marja.marinic@gmail.com>
2023-05-01 22:31:17 +0100

    Madlib

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+activity = input("Activity: ")
+feeling = input("Feeling: ")
+adj = input("Adjective: ")
+adj2 = input("Adjective: ")
+famous_person = input("Famous person: ")
+feeling2 = input("Feeling: ")
+
+
+madlib = f"Do you like to {activity}? I just {feeling} it! It is not only {adj} but also {adj2}. \
+Imagine what {famous_person} told me about that... he {feeling2} it!"
+
+print(madlib)
